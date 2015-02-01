3d6f36bafc2b3d818f6ba077511e66a3634157ce
Boris Scheiman <bscheiman@gmail.com>
2015-02-01 14:32:53 -0600

    BindablePicker

---
+++ AppCreator/Controls/BindablePicker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppCreator.Controls {
+	public class BindablePicker<T> : Picker {
+		public BindablePicker() {
+			SelectedIndexChanged += OnSelectedIndexChanged;
+		}
+
+		public IEnumerable<T> ItemsSource { get; set; }
+		public T SelectedItem { get; set; }
+
+		private static void OnItemsSourceChanged(BindableObject bindable, IEnumerable oldvalue, IEnumerable newvalue) {
+			var picker = bindable as BindablePicker<T>;
+			picker.Items.Clear();
+
+			if (newvalue != null) {
+				foreach (var item in newvalue) {
+					picker.Items.Add(item.ToString());
+				}
+			}
+		}
+
+		private void OnSelectedIndexChanged(object sender, EventArgs eventArgs) {
+			if (SelectedIndex < 0 || SelectedIndex > Items.Count - 1) {
+				SelectedItem = default(T);
+			} else {
+				SelectedItem = ItemsSource.ElementAt(SelectedIndex);
+			}
+		}
+		private static void OnSelectedItemChanged(BindableObject bindable, object oldvalue, object newvalue) {
+			var picker = bindable as BindablePicker<T>;
+
+			if (newvalue != null)
+				picker.SelectedIndex = picker.Items.IndexOf(newvalue.ToString());
+		}
+	}
+}
+
