7a76f1c53eb5d3792b10625732435e7f6e65c93a
Boris Scheiman <bscheiman@gmail.com>
2015-02-01 14:48:48 -0600

    Quickfix

--- AppCreator/Controls/BindablePicker.cs
+++ AppCreator/Controls/BindablePicker.cs
@@ -3,15 +3,45 @@ using Xamarin.Forms;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Specialized;
 
 namespace AppCreator.Controls {
 	public class BindablePicker<T> : Picker {
+		public static BindableProperty ItemsSourceProperty = BindableProperty.Create<BindablePicker<T>, IEnumerable>(o => o.ItemsSource,
+			                                                     default(IEnumerable), propertyChanged: OnItemsSourceChanged);
+
+		public static BindableProperty SelectedItemProperty = BindableProperty.Create<BindablePicker<T>, object>(o => o.SelectedItem,
+			                                                      default(object), propertyChanged: OnSelectedItemChanged);
+
+
+		public IEnumerable<T> ItemsSource {
+			get { return (IEnumerable<T>)GetValue(ItemsSourceProperty); }
+			set {
+				var notifyChanged = value as INotifyCollectionChanged;
+
+				if (notifyChanged != null) {
+					var obj = notifyChanged;
+
+					obj.CollectionChanged += (sender, e) => {
+						Items.Clear();
+
+						foreach (var i in (value as ICollection))
+							Items.Add(i.ToString());
+					};
+				}
+
+				SetValue(ItemsSourceProperty, value);
+			}
+		}
+
+		public T SelectedItem {
+			get { return (T)GetValue(SelectedItemProperty); }
+			set { SetValue(SelectedItemProperty, value); }
+		}
+
 		public BindablePicker() {
 			SelectedIndexChanged += OnSelectedIndexChanged;
 		}
-
-		public IEnumerable<T> ItemsSource { get; set; }
-		public T SelectedItem { get; set; }
 
 		private static void OnItemsSourceChanged(BindableObject bindable, IEnumerable oldvalue, IEnumerable newvalue) {
 			var picker = bindable as BindablePicker<T>;
