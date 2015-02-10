75637d7a8672a303dbfe0fc209e3488d9844b0c9
Boris Scheiman <bscheiman@gmail.com>
2015-02-10 07:26:33 -0600

    1.2.16

--- publish.bat
+++ publish.bat
@@ -1,12 +1,16 @@
 @echo off
 IF "%~1"=="" GOTO ERROR
+cls
 
-@REM "%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" /p:Configuration=Release /t:Clean /t:Rebuild /m
+"%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" /p:Configuration=Release /t:Clean /t:Rebuild
 del *.nupkg
 .nuget\NuGet.exe pack AppCreator.nuspec -Version %1
 .nuget\NuGet.exe pack AppCreator.UI.nuspec -Version %1
 
 for %%i in (AppCreator*.nupkg) do .nuget\NuGet.exe push %%i %NUGET%
+goto DONE
 
 :ERROR
 echo Please specify version
+
+:DONE
