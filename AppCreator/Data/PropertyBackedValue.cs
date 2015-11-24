e727403c0029348a029528dca8f07dfa8f13901f
Boris Scheiman <bscheiman@gmail.com>
2015-11-24 15:32:57 -0600

    PropertyBackedValue

---
+++ AppCreator/Data/PropertyBackedValue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace AppCreator.Data {
+	public class PropertyBackedValue<T> {
+		internal string Key { get; set; }
+		internal bool Found { get; set; }
+		internal T Value { get; set; }
+
+		public PropertyBackedValue(string key, T val = default(T)) {
+			if (string.IsNullOrEmpty(key))
+				throw new ArgumentNullException(nameof(key));
+
+			Key = key;
+			Found = Application.Current.Properties.ContainsKey(key);
+
+			Value = Found ? (T) Application.Current.Properties[key] : val;
+
+			Application.Current.SavePropertiesAsync();
+		}
+
+		public T Get() {
+			return Value;
+		}
+
+		public void Set(T val) {
+			Value = val;
+			Application.Current.SavePropertiesAsync();
+		}
+
+		public static implicit operator T(PropertyBackedValue<T> tpb) {
+			return tpb.Get();
+		}
+	}
+}
+
